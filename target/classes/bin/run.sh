6e2c429f6493a41e45f43800caf5391b36670889
jamesj <ehddnr177@uangel.com>
2021-10-01 14:56:04 +0900

    gitignore reapply

--- target/classes/bin/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -jar VOIP_Phone.jar VoipPhoneMain user_conf.ini
--- target/classes/lib/evs/src/decoder.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DECODER_H__
-#define __DECODER_H__
-
-#include <stdio.h>
-
-void decode( int argc, char** argv, FILE *f_stream, FILE *f_synth );
-
-#endif
-
